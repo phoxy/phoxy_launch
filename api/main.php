e78d69d6ec940f82ae04334589a2f79185efa188
Kirill Berezin <enelar@develop-project.ru>
2013-07-28 15:58:46 +0400

    Understanding of `public` and `protected` phoxy modules context. url to testing: http://localhost/api/main/FUNCTION ex: http://localhost/api/main/_Protected http://localhost/api/main/Reserve http://localhost/api/main/LocalCall

--- api/main.php
+++ api/main.php
@@ -6,4 +6,39 @@ class main extends api
   {
     return array("error" => "Test function call");
   }
+  
+  protected function _Protected()
+  {
+    return array("data" => "This function could be reached via REST RPC(over HTTP+JSON), and via local calls");
+  }
+  
+  protected function LocalCall()
+  { 
+    $test = LoadModule('api', 'main');
+    $res = $test->_Protected();
+    return array("data" => var_export($res, true));
+  }
+
+  public function _Public()
+  {
+    return "This function unreached via RCP, but could be access with LoadModule. ".
+    "Never use include, etc files with api modules, except if you php guru.";
+  }
+  
+  private function _Private()
+  {
+    return 'This function could be called only directly with "$this" variable';
+  }
+  
+  protected function Complex()
+  {
+    return array("data" => array("comment" => "complex results returns as array", "istrue" => "yes"));
+  }
+  
+  protected function ComplexCall()
+  {
+    $test = LoadModule('api', 'main');
+    $res = $test->Complex();
+    return array("error" => var_export($res, true));
+  }
 }
